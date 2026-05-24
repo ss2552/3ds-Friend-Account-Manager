061b8e58938af756224486b74d998d6db051dbc1
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-05-24 21:11:26 +0900

    Update main.cpp

--- app/source/main.cpp
+++ app/source/main.cpp
@@ -1,31 +1,22 @@
-#include "./frda.h"
+// #include "./frda.h"
+#include <3ds/services/frd.h>
 #include <3ds.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#define handleResult(action)                          \
-  Result rc = action;                                 \
-  if (R_FAILED(rc)) {                                 \
-    printf("Error: %08lx\n\n", rc);                   \
-  } else {                                            \
-    hasTakenAction = true;                            \
-    printf("Success!  Will reboot on \"Start\"\n\n"); \
-  }
 
 int main(int argc, char *argv[]) {
 
   int const pretendo_act = 2;
   
   nsInit();
-  frdAInit();
+  // frdAInit();
+  frdInit();
   gfxInitDefault();
   consoleInit(GFX_TOP, NULL);
 
   // This version or higher is required creating/swaping local accounts
   FRDA_SetClientSdkVersion(0x70000c8);
-
-  bool hasTakenAction = false;
 
   printf("Friend account manager\n");
   printf("Press \"Start\" to quit or reboot\n");
@@ -40,11 +31,9 @@ int main(int argc, char *argv[]) {
     // Your code goes here
     u32 kDown = hidKeysDown();
     if (kDown & KEY_START) {
-      if (hasTakenAction) {
-        NS_RebootSystem();
-      } else {
-        break;  // break in order to return to hbmenu
-      }
+      NS_RebootSystem();
+    } else {
+      break;  // break in order to return to hbmenu
     }
 
     if (kDown & KEY_X) {
@@ -55,7 +44,7 @@ int main(int argc, char *argv[]) {
   }
 
   gfxExit();
-  frdAExit();
+  frdExit();
   nsExit();
   return 0;
 }
