b18a0fb4d29e119915f612afed8d9dfbd80b35fd
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-05-24 21:24:32 +0900

    Update main.cpp

--- app/source/main.cpp
+++ app/source/main.cpp
@@ -12,38 +12,12 @@ int main(int argc, char *argv[]) {
   nsInit();
   // frdAInit();
   frdInit();
-  gfxInitDefault();
-  consoleInit(GFX_TOP, NULL);
 
   // This version or higher is required creating/swaping local accounts
-  FRDA_SetClientSdkVersion(0x70000c8);
+  FRD_SetClientSdkVersion(0x70000c8);
 
-  printf("Friend account manager\n");
-  printf("Press \"Start\" to quit or reboot\n");
-  printf("Press \"X\" to delete account 2\n");
-
-  // Main loop
-  while (aptMainLoop()) {
-    gspWaitForVBlank();
-    gfxSwapBuffers();
-    hidScanInput();
-
-    // Your code goes here
-    u32 kDown = hidKeysDown();
-    if (kDown & KEY_START) {
-      NS_RebootSystem();
-    } else {
-      break;  // break in order to return to hbmenu
-    }
-
-    if (kDown & KEY_X) {
-      printf("Using local account Id 1...\n");
-      handleResult(FRDA_DeleteLocalAccount(pretendo_act));
-    }
-
-  }
-
-  gfxExit();
+  handleResult(FRDA_DeleteLocalAccount(pretendo_act));
+  
   frdExit();
   nsExit();
   return 0;
