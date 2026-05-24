796a8c258c9f2adcc3293aef4825899f1acf2ba4
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-05-24 21:00:35 +0900

    Update frda.c

--- app/source/frda.c
+++ app/source/frda.c
@@ -18,68 +18,37 @@ and modified to:
 static Handle frdHandle;
 static int frdRefCount;
 
-Result frdAInit() {
-  Result ret = 0;
+Result frdInit(bool forceUser)
+{
+	Result ret = 0;
 
-  if (AtomicPostIncrement(&frdRefCount))
-    return 0;
+	if (AtomicPostIncrement(&frdRefCount)) return 0;
 
-  ret = srvGetServiceHandle(&frdHandle, "frd:a");
-  if (R_FAILED(ret))
-    AtomicDecrement(&frdRefCount);
+	if (forceUser)
+		ret = srvGetServiceHandle(&frdHandle, "frd:u");
+	else
+		ret = srvGetServiceHandle(&frdHandle, "frd:a");
 
-  return ret;
+	if (R_FAILED(ret))
+		AtomicDecrement(&frdRefCount);
+
+	return ret;
 }
 
-void frdAExit() {
-  if (AtomicDecrement(&frdRefCount))
-    return;
-  svcCloseHandle(frdHandle);
+void frdExit(void)
+{
+	if (AtomicDecrement(&frdRefCount)) return;
+	svcCloseHandle(frdHandle);
 }
 
-Handle *frdAGetSessionHandle(void) {
-  return &frdHandle;
-}
+Result FRDA_DeleteLocalAccount(u8 localAccountId)
+{
+	Result ret = 0;
+	u32 *cmdbuf = getThreadCommandBuffer();
+	cmdbuf[0] = IPC_MakeHeader(0x402,1,0); // 0x4020040
+	cmdbuf[1] = (u32)localAccountId;
 
-Result FRDA_CreateLocalAccount(u8 localAccountId, NascEnvironment nascEnvironment, u8 serverTypeField1, u8 serverTypeField2) {
-  Result ret = 0;
-  u32 *cmdbuf = getThreadCommandBuffer();
+	if (R_FAILED(ret = svcSendSyncRequest(frdHandle))) return ret;
 
-  cmdbuf[0] = IPC_MakeHeader(0x401, 4, 0);
-  cmdbuf[1] = localAccountId;
-  cmdbuf[2] = nascEnvironment;
-  cmdbuf[3] = serverTypeField1;
-  cmdbuf[4] = serverTypeField2;
-
-  if (R_FAILED(ret = svcSendSyncRequest(frdHandle)))
-    return ret;
-
-  return (Result)cmdbuf[1];
-}
-
-Result FRDA_SetLocalAccountId(u8 localAccountId) {
-  Result ret = 0;
-  u32 *cmdbuf = getThreadCommandBuffer();
-
-  cmdbuf[0] = IPC_MakeHeader(0x403, 1, 0);
-  cmdbuf[1] = localAccountId;
-
-  if (R_FAILED(ret = svcSendSyncRequest(frdHandle)))
-    return ret;
-
-  return (Result)cmdbuf[1];
-}
-
-Result FRDA_SetClientSdkVersion(u32 sdkVer) {
-  Result ret = 0;
-  u32 *cmdbuf = getThreadCommandBuffer();
-
-  cmdbuf[0] = IPC_MakeHeader(0x32, 1, 2);
-  cmdbuf[1] = sdkVer;
-  cmdbuf[2] = IPC_Desc_CurProcessId();
-
-  if (R_FAILED(ret = svcSendSyncRequest(frdHandle)))
-    return ret;
-
-  return (Result)cmdbuf[1];
+	return (Result)cmdbuf[1];
 }
