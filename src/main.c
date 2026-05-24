76a04c08ba86f13a4b66d1af51e8c7577a2b2fb3
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-05-24 22:37:40 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define handleResult(action)                          \
+  Result rc = action;                                 \
+  if (R_FAILED(rc)) {                                 \
+    printf("Error: %08lx\n\n", rc);                   \
+  } else {                                            \
+    hasTakenAction = true;                            \
+    printf("Success!  Will reboot on \"Start\"\n\n"); \
+  }
+
+int main() {
+  frdInit(false);
+  gfxInitDefault();
+  consoleInit(GFX_TOP, NULL);
+
+  // This version or higher is required creating/swaping local accounts
+  FRDA_SetClientSdkVersion(0x70000c8);
+  
+  int const pretendo_act = 2;
+
+  consoleClear();
+  
+  printf("please to X_key  ¥n");
+  
+  // Main loop
+  while (aptMainLoop()) {
+    hidScanInput();
+
+    if (kDown & KEY_X) {
+      handleResult(FRDA_DeleteLocalAccount(pretendo_act));
+    }
+  }
+
+  gfxExit();
+  frdExit();
+  return 0;
+}
+
+/*
 int main(){
 
   int const pretendo_act = 2;
@@ -15,3 +53,4 @@ int main(){
   return 0;
   
 }
+*/
