c3892fff4b6ac1bcc325195902490b929bf19ffe
えす ESU <85072753+ss2552@users.noreply.github.com>
2026-05-24 22:28:40 +0900

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,5 +1,3 @@
-// #include "./frda.h"
-// #include <3ds/services/frd.h>
 #include <3ds.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,12 +8,10 @@ int main(){
   int const pretendo_act = 2;
 
   frdInit(false);
-
-  // This version or higher is required creating/swaping local accounts
-  // FRD_SetClientSdkVersion(0x70000c8);
-
+  FRD_SetClientSdkVersion(0x70000c8);
   FRDA_DeleteLocalAccount(pretendo_act);
-  
   frdExit();
+  
   return 0;
+  
 }
